b2aa2366a001aea612c7cd1e14dfa31ebb3eabf0
isabelmorigeau <morigeau@uleth.ca>
2023-12-07 16:03:58 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+console.log("Hello from scripts.js!");
+
+$('bitoon').on("click", function() {
+    $('#statement').html('You have reached a fork in the path! Which way do you chose?')
+    $('#picture').html('<img src="images/imageOne.png">')
+});
