a7d0a56c6644dfa3a1a166f85bb222c41bd37ddd
isabelmorigeau <morigeau@uleth.ca>
2023-12-11 16:10:37 -0700

    com

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 console.log("Hello from scripts.js!");
 
-$('bitoon').on("click", function() {
+document.getElementById("buttonTwo").onclick = function() {
+    document.getElementById("buttonOne").style.visibility = "visible";
+}
+
+$('.bitoon').on("click", function() {
     $('#statement').html('You have reached a fork in the path! Which way do you chose?')
     $('#picture').html('<img src="images/imageOne.png">')
+    $('#buttonOne').html('Dark Path')
 });
+
