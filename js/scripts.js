1bfeb15059f941a425b1a6b4ae61b141cc3fd155
isabelmorigeau <morigeau@uleth.ca>
2023-12-11 16:18:02 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,19 @@ console.log("Hello from scripts.js!");
 document.getElementById("buttonTwo").onclick = function() {
     document.getElementById("buttonOne").style.visibility = "visible";
 }
+document.getElementById("buttonTwo").onclick = function() {
+    document.getElementById("buttonThree").style.visibility = "visible";
+}
+
+document.getElementById("buttonTwo").onclick = function() {
+    document.getElementById("picture").style.visibility = "visible";
+}
 
 $('.bitoon').on("click", function() {
     $('#statement').html('You have reached a fork in the path! Which way do you chose?')
     $('#picture').html('<img src="images/imageOne.png">')
     $('#buttonOne').html('Dark Path')
+    $('#buttonTwo').html('Light Path')
+    $('#buttonThree').html('Rocky Path')
 });
 
